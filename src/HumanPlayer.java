ab5971334e7eda28654e0177effabb735e6b2bdb
kimdonguk-erica <109131077+kimdonguk-erica@users.noreply.github.com>
2022-12-01 12:38:28 +0900

    1차 수정

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,4 +1,34 @@
 
-public class HumanPlayer {
+public abstract class HumanPlayer extends CardPlayer{
+private String name;
+private int chips;
 
+	public HumanPlayer(int max_cards, String n) {
+		super(max_cards);
+		n = name;
+	}
+	public void win() {
+		chips = chips + 3;
+	}
+	public void loose() {
+		chips = chips - 1;
+	}
+	public int chips() {
+		return chips;1
+	}
+	public Card[] hand() {
+		return super.hand();
+	}
+	public String name() {
+		return name;
+	}
+	public boolean possible(Card put) {
+		if(possible_cards(put).length > 0)
+			return true;
+		else
+			return false;
+	}
+	public Card[] choice_card(Card[] possible_cards, Card put) {
+		return super.possible_cards(put);
+	}
 }
