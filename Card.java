3965046eb4db448abcbceb0ba5767ae186da1e4d
doyoon323 <115680101+doyoon323@users.noreply.github.com>
2022-12-01 11:14:04 +0900

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,7 +1,48 @@
 
-public class Card implements CardInterface{
-	public Card(String s, int r) {}
-	public String suit() {
-		return null;
-	}
+public class Card {
+	
+	public static final String SPADES = "spades";
+    public static final String HEARTS = "hearts";
+    public static final String DIAMONDS = "diamonds";
+    public static final String CLUBS = "clubs";
+
+    
+    public static final int ACE = 1;
+    public static final int JACK = 11;
+    public static final int QUEEN = 12;
+    public static final int KING = 13;
+    public static final String Black_Joker = "blackjocker";
+    public static final String Color_Joker = "colorjocker";
+
+    
+    private String suit;
+    private int rank;
+    
+    public Card(String s, int r) {
+        suit = s; 
+        rank = r;
+    }
+    
+    public String suit() {
+        return suit;
+    }
+
+    public int rank() {
+        return rank;
+    }
+    
+
+    //특정카드와 모양이같은경우 
+    public boolean shape_equals(Card c) {
+        return suit.equals(c.suit());
+    }
+    
+    //특정카드와 숫자가 같은 경우 
+    public boolean num_equals(Card c) {
+    	return rank == c.rank();
+    }
+    
 }
+
+
+
